d70aeeeb9d6c797553bc2be37c4732ecf999210d
hoapham236 <pnhpy2306@gmail.com>
2025-09-04 10:36:38 +0700

    check fix

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import { assets } from "@/assets/assets";
 import productService from "../services/productService.js";
 import categoryService from "../services/categoryService.js";
