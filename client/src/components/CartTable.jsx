feac72fb4874792f06c9b1ece5e5d42ce8d7a006
MunCruella <nhunguyetpy206@gmail.com>
2025-09-10 02:33:14 +0700

    Thêm thanh cuốn cho phần cart

--- client/src/components/CartTable.jsx
+++ client/src/components/CartTable.jsx
@@ -4,36 +4,39 @@ import CartItemRow from "./item/CartItemRow";
 const CartTable = ({ cartItems, updateQuantity, removeFromCart }) => {
   return (
     <div className="overflow-x-auto shadow rounded-xl bg-white">
-      <table className="w-full text-left">
-        <thead className="bg-green-700">
-          <tr>
-            <th className="py-3 px-4 text-white">Products</th>
-            <th className="py-3 px-4 text-white">Quantity</th>
-            <th className="py-3 px-4 text-right text-white">Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(cartItems) && cartItems.length > 0 ? (
-            cartItems.map((item) => (
-              <CartItemRow
-                key={item._id}
-                item={item}
-                updateQuantity={updateQuantity}
-                removeFromCart={removeFromCart}
-              />
-            ))
-          ) : (
+      {/* Giới hạn chiều cao + thêm overflow-y-auto để có thanh lăn dọc */}
+      <div className="max-h-120 overflow-y-auto">
+        <table className="w-full text-left">
+          <thead className="bg-green-700 sticky top-0 z-10">
             <tr>
-              <td
-                colSpan="3"
-                className="py-6 text-center text-gray-500 font-medium"
-              >
-                Your cart is empty.
-              </td>
+              <th className="py-3 px-4 text-white">Products</th>
+              <th className="py-3 px-4 text-white">Quantity</th>
+              <th className="py-3 px-4 text-right text-white">Subtotal</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {Array.isArray(cartItems) && cartItems.length > 0 ? (
+              cartItems.map((item) => (
+                <CartItemRow
+                  key={item._id}
+                  item={item}
+                  updateQuantity={updateQuantity}
+                  removeFromCart={removeFromCart}
+                />
+              ))
+            ) : (
+              <tr>
+                <td
+                  colSpan="3"
+                  className="py-6 text-center text-gray-500 font-medium"
+                >
+                  Your cart is empty.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
